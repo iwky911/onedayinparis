dcb105caac97f994f85aa19690b08e0a92e53b27
bertrand simonnet <iwky911@gmail.com>
2012-07-27 19:57:31 +0200

    adding diversification strategy

--- src/tsp/operators.go
+++ src/tsp/operators.go
@@ -4,47 +4,46 @@ import(
 	"fmt"
 )
 
-func (a AffectData) swap(i, j int) AffectData {
-	b := AffectData(make([]int, len(a)))
-	copy(b, a)
-	b[i], b[j] = a[j], a[i]
-	return b
+func (a AffectData) swap(i, j int, b *AffectData ) {
+	copy(*b, a)
+	//~ println(i,j)
+	(*b)[i], (*b)[j] = a[j], a[i]
 }
 
-func (a AffectData) invertRange(i, j int) AffectData {
+func (a AffectData) invertRange(i, j int, b *AffectData ) {
 	if j < i {
-		return a.invertRange(j, i)
+		a.invertRange(j, i, b)
+		return
 	}
-	b := AffectData(make([]int, len(a)))
-	copy(b, a)
+	copy(*b, a)
 	for k := 0; k <= j-i; k++ {
-		b[i+k] = a[j-k]
+		(*b)[i+k] = a[j-k]
 	}
-	return b
 }
 
-func (a AffectData) cutAt(i int) AffectData {
+func (a AffectData) cutAt(i int, b *AffectData ){
 	n := len(a)
-	b := AffectData(make([]int, n))
 
 	for k := 0; k < i; k++ {
-		b[n-i+k] = a[k]
+		(*b)[n-i+k] = a[k]
 	}
 	for k := 0; k < n-i; k++ {
-		b[k] = a[i+k]
+		(*b)[k] = a[i+k]
 	}
-	return b
 }
 
-func (a Affectation) swap(i, j int) Affectation {
-	return Affectation{a.data.swap(i,j), fmt.Sprintf("swap %d %d", i,j)}
+func (a Affectation) swap(i, j int, b *Affectation) {
+	a.data.swap(i,j, &(*b).data)
+	(*b).name=fmt.Sprintf("swap %d %d", i,j)
 }
 
-func (a Affectation) invertRange(i, j int) Affectation {
-	return Affectation{a.data.invertRange(i,j), fmt.Sprintf("invert range %d %d", i,j)}
+func (a Affectation) invertRange(i, j int, b *Affectation) {
+	a.data.invertRange(i,j, &(*b).data)
+	(*b).name=fmt.Sprintf("invert range %d %d", i,j)
 }
 
-func (a Affectation) cutAt(i int) Affectation {
+func (a Affectation) cutAt(i int, b *Affectation) {
 	//~ println("in cut at", i)
-	return Affectation{a.data.cutAt(i), fmt.Sprintf("cut at %d", i)}
+	a.data.cutAt(i, &(*b).data)
+	(*b).name=fmt.Sprintf("cut at %d", i)
 }
--- src/tsp/operators_test.go
+++ src/tsp/operators_test.go
@@ -20,15 +20,16 @@ import (
 
 func TestRevert(t *testing.T) {
 	a := AffectData([]int{0, 1, 2, 3})
-	a = a.invertRange(0, 3)
-	if !reflect.DeepEqual(a, AffectData([]int{3, 2, 1, 0})) {
+	b := AffectData([]int{0, 1, 2, 3})
+	a.invertRange(0, 3, &b)
+	if !reflect.DeepEqual(b, AffectData([]int{3, 2, 1, 0})) {
 		t.Fail()
 	}
 }
 
 func TestCostComputationGiveCostSum(t *testing.T) {
 	a := AffectData([]int{0, 1, 2})
-	p := Problem{&[][]int{{0, 1, 2}, {2, 0, 2}, {1, 1, 0}}}
+	p := Problem{[][]int{{0, 1, 2}, {2, 0, 2}, {1, 1, 0}}}
 	if p.Cost(&Affectation{a,""}) != 3 {
 		t.Fail()
 	}
@@ -47,7 +48,8 @@ func TestChooseSmallerCost(t *testing.T) {
 
 func TestOperatorDoNotChangeFirstAffectData(t *testing.T) {
 	var s = AffectData([]int{0, 1, 2})
-	b := s.swap(1, 2)
+	b := AffectData([]int{0,0,0})
+	s.swap(1, 2, &b)
 	if s[1] == 2 && b[1] == 2 {
 		t.Errorf("swap modified s")
 	}
@@ -55,12 +57,14 @@ func TestOperatorDoNotChangeFirstAffectData(t *testing.T) {
 
 func TestCutWorks(t *testing.T) {
 	a := AffectData([]int{1, 2, 3})
-	b := a.cutAt(2)
+	b := AffectData([]int{1, 2, 3})
+	a.cutAt(2, &b)
 	if !reflect.DeepEqual(b, AffectData([]int{ 3, 1, 2})) {
 		fmt.Println(a, b)
 		t.Errorf("not equals! ")
 	}
-	c := b.cutAt(1)
+	c:= AffectData([]int{1, 2, 3})
+	b.cutAt(1, &c)
 	if ! reflect.DeepEqual(a,c) {
 		t.Errorf("applied twice doesn't produce the same result")
 	}
