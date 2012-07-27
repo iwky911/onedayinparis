e3773af87ac17a4f79c5554125454a8ed88b05ac
bertrand simonnet <iwky911@gmail.com>
2012-07-27 10:28:55 +0200

    adding matrix computation program

---
+++ matrix_computation/priorityqueue.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"container/heap"
+)
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    int     // The value of the item; arbitrary.
+	priority int // The priority of the item in the queue.
+	// The index is needed by changePriority and is maintained by the heap.Interface methods.
+	index      int   // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	a := *pq
+	n := len(a)
+	a = a[0 : n+1]
+	item := x.(*Item)
+	item.index = n
+	a[n] = item
+	*pq = a
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	a := *pq
+	n := len(a)
+	item := a[n-1]
+	item.index = -1 // for safety
+	*pq = a[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) AddNode(item *Item) {
+	heap.Push(pq, item)
+}
